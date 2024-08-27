a2d3976f9b222662a40a465acb0d798f4a9d72d2
shashitha2002 <130375872+shashitha2002@users.noreply.github.com>
2024-08-27 15:28:20 +0530

    modify product model

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
 import express  from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
-import cors from 'cors'
-import bodyParser from "body-parser"
+import cors from 'cors';
+import bodyParser from "body-parser";
 
 dotenv.config()
 
 const app = express()
 
 app.use(helmet())
-app.use(cors)
-app.use(express.json)
-app.use(bodyParser.json)
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
 
 app.get('/', (req, res)=> {
     res.send("Spring Bing backend on notch")
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
